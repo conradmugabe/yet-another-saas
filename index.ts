7a19e48ee5e97d044fc497b64f1fd3b177f8c80e
conrad-evans <conradmugabe@gmail.com>
2022-06-27 02:56:33 +0300

    added express web server to the application

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Setup is complete!");
+import express from "express";
+import compression from "compression";
+
+const app = express();
+app.use(express.json());
+app.use(compression());
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
