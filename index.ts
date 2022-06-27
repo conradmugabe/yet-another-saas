085c306fe5c3991c5dfa624fb1a36ca13a947625
conrad-evans <conradmugabe@gmail.com>
2022-06-27 12:38:12 +0300

    exposed blog routes to application

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import compression from "compression";
+import { blogRouter } from "./routes/blog.routes";
 
 const app = express();
 app.use(express.json());
 app.use(compression());
+app.use("/", blogRouter);
 
 const port = process.env.PORT || 3000;
 
